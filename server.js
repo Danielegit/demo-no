c8c8bff46ee2572a7263148bf4c76f185fc90472
Daniele Argiolas <danieleargiolas9@gmail.com>
2020-05-11 16:40:41 +0200

    8080

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __di
 app.set('view engine', 'hbs');
 
 // PORT 8080
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port,ip)
 
 //Routes
